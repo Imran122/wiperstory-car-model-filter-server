452f495402ac404597f174ebf738b06f19e7c5e8
ghost <imransardar122@gmail.com>
2021-12-05 23:33:41 +0600

    fix auth error

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ const ObjectId = require('mongodb').ObjectId;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdztw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//new
 
-//connect to db
-/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvy8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); */
 
 
 
@@ -27,14 +23,40 @@ async function run() {
         await client.connect();
         const database = client.db('wiperStory');
         const projectCollection = database.collection('carListDb');
-        //colection for insert place order data
+        const usersCollection = database.collection('users')
+
 
         //GET APi to get data 
 
 
+        //save email pass auth data to mongodb
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.json(result);
+        })
 
+        //api for make admin
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email }
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.json(result);
+        })
 
-
+        //api for check user admin or not for admin private route
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            console.log(isAdmin);
+            res.json({ admin: isAdmin });
+        })
 
         console.log('db connected');
     } finally {
