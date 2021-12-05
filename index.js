faf4f97120ba8f32d9051001244c0604db93eeac
ghost <imransardar122@gmail.com>
2021-12-05 17:21:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+app.use(cors());
+app.use(express.json());
+//single data loading by objectId
+const ObjectId = require('mongodb').ObjectId;
+
+//configure midleware cors
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdztw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//new
+
+//connect to db
+/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvy8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); */
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('wiperStory');
+        const projectCollection = database.collection('carListDb');
+        //colection for insert place order data
+
+        //GET APi to get data 
+
+
+
+
+
+
+        console.log('db connected');
+    } finally {
+
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
